174673ec998abb2b42f0ae8d755441ae715b6456
Rick Houser <frakentoaster@gmail.com>
2016-02-12 08:06:03 +0000

    print_number WIP

--- dreams_and_dedication_are_a_powerful_combination/0-main.c
+++ dreams_and_dedication_are_a_powerful_combination/0-main.c
@@ -2,6 +2,7 @@
 
 int main(void)
 {
+
   print_number(98);
   print_char('\n');
   print_number(402);
@@ -12,5 +13,11 @@ int main(void)
   print_char('\n');
   print_number(-98);
   print_char('\n');
-  return (0);
+  print_number(-44498);
+  print_char('\n');
+  print_number(-980);
+  print_char('\n');
+  print_number(-198);
+  print_char('\n');
+   return (0);
 }
--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,43 +1,42 @@
 #include "my_functions.h"
+#include <stdio.h>
+
+int myPow(int power)
+{
+  int result;
+  int i;
+
+  for (i = 0; i < power; i++)
+  {
+    result *= 10;
+  }
+  return result;
+}
 
 void print_number(int n)
 {
-	int l=1;
-	int x, p;
-	if (n < 0)
-	{
-		print_char(45);
-		n = (-n*-n);
-	}
+  int len = 1;
+  int p;
+  int x;
+  p = len-1;
 
-	while (n > 9)
-	{
-		l++;
-		n/=10;
-	}
-	
-	while (n > 9)
-	{
-		p = l-1;
-		x = n / 10**p;
-		n = n - x * 10**p;
-		print_char(x + 48);
-		p--;
-	}
-
-	if (n > 0 && n < 10) 
-	{
-		print_char(n + 48);
-	}
-
-	if (n == 0)
-	{
-		print_char(48);
-	}
-}
-
-int myPow(int a, int b)
-{
-	int a;
-	int 
+  if (n < 0)
+    {
+      print_char(45);
+      n = (n*-1);
+    }
+  
+  while (n > 9)
+    {
+      len++;
+      n/=10;
+    }
+  
+  while (p >= 0)
+    {
+      x = n / myPow(p);
+      n = n - x * myPow(p);
+      print_char(x + 48);
+      p--;
+    }
 }
