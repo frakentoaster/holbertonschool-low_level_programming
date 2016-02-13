6f4fdec201d4f920e6015b6ea9064269c485e834
Rick Houser <frakentoaster@gmail.com>
2016-02-13 02:43:23 +0000

    Finish 0-print_number.c meeting all constraints =)

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -1,60 +1,50 @@
 #include "my_functions.h"
 
-int myPow(int power)
-{
+void int_min(){ /* Function to handle int min */
+  print_char(45);
+  print_char(50);
+  print_char(49);
+  print_char(52);
+  print_char(55);
+  print_char(52);
+  print_char(56);
+  print_char(51);
+  print_char(54);
+  print_char(52);
+  print_char(56);
+}
+
+int myPow(int power){ /* Function to calculate exponents */
   int i;
   int result;
   result = 1;
 
-  for (i = 0; i < power; i++)
-  {
+  for (i = 0; i < power; i++) {
     result *= 10;
   }
   return result;
 }
 
-void print_number(int n)
-{
-  int len;
-  int power;
-  int num;
-  int temp;
+void print_number(int n) {
+  int len, power, num, temp;
   len = 1;
  
-  if (n == -2147483648)
-  {
-    print_char(45);
-    print_char(50);
-    print_char(49);
-    print_char(52);
-    print_char(55);
-    print_char(52);
-    print_char(56);
-    print_char(51); 
-    print_char(54);
-    print_char(52);
-    print_char(56);
+  if (n == -2147483648) { /* Call int_min function */
+    int_min();
   }
-
-  if (n < 0 && n !=-2147483648)
-  {
-    print_char(45);
-    n = (n * -1);
+  /* Prepend "-" char and convert neg to pos */
+  if (n < 0 && n !=-2147483648) {
+    print_char(45); n = (n * -1);
   }
   temp = n;
 
-  while (temp > 9)
-  {
-      len++; 
-      temp /= 10;
+  while (temp > 9){ /* Calculate length of numbers */
+      len++; temp /= 10;
   }
   power = len-1;
 
-  while (power >= 0 && n !=-2147483648)
-  {
-    num = n / myPow(power);
-    n = n - num * myPow(power);
-    print_char(num + 48);
-    power--;
+  while (power >= 0 && n !=-2147483648){ /* Print each ascii char */
+    num = n / myPow(power); n = n - num * myPow(power);
+    print_char(num + 48); power--;
   }
 }
