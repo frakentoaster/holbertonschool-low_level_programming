721766364f3227d3a73d29b0ed753e9e8ce072a9
Rick Houser <frakentoaster@gmail.com>
2016-02-13 00:38:32 +0000

    complete 0-print_numbers!

--- dreams_and_dedication_are_a_powerful_combination/0-print_number.c
+++ dreams_and_dedication_are_a_powerful_combination/0-print_number.c
@@ -19,12 +19,27 @@ void print_number(int n)
   int power;
   int num;
   int temp;
-
   len = 1;
-  if (n < 0)
+ 
+  if (n == -2147483648)
   {
-      print_char(45);
-      n = (n * -1);
+    print_char(45);
+    print_char(50);
+    print_char(49);
+    print_char(52);
+    print_char(55);
+    print_char(52);
+    print_char(56);
+    print_char(51); 
+    print_char(54);
+    print_char(52);
+    print_char(56);
+  }
+
+  if (n < 0 && n !=-2147483648)
+  {
+    print_char(45);
+    n = (n * -1);
   }
   temp = n;
 
@@ -35,7 +50,7 @@ void print_number(int n)
   }
   power = len-1;
 
-  while (power >= 0)
+  while (power >= 0 && n !=-2147483648)
   {
     num = n / myPow(power);
     n = n - num * myPow(power);
