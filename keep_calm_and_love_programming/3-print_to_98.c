1c78f61ad109d587779f2de48b8825836393e054
Rick Houser <frakentoaster@gmail.com>
2016-02-19 01:44:00 +0000

    add function to print nums to 98

---
+++ keep_calm_and_love_programming/3-print_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+  while (n < 98)
+    {
+      printf("%d, ",n);
+      n++;
+    }
+  while (n > 98)
+    {
+      printf("%d, ",n);
+      n--;
+    }
+  printf("%d\n",n);
+}
