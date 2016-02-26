27e3bd33affce1dfc714e1413c90e6367c1790e3
Rick Houser <frakentoaster@gmail.com>
2016-02-26 04:27:29 +0000

    finish printing trees

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -3,27 +3,30 @@
 void triangles_in_the_term(int h, int n)
 {
   /* Declare variables */
-  int spaces, stars, row;
+  int spaces, stars, row, i;
 
   /* Execute code only if height > 0 */
   if (h > 0 && n > 0)
     {
-      /* Build pyramid */
-      for (row = 0; row <= h-1; row++)
-        {
-          /* Print spaces */
-          for (spaces = 0; spaces < (h-row-1); spaces++)
-            { 
-              printf(" ");
-            }
-
-          /* Print asterisks */
-          for (stars = 1; stars < (h-spaces)*2; stars++)
-            {
-              printf("*");
-            }
-
-          printf("\n");
-        }
+      for (i = 0; i < n; i++)
+	{
+	  /* Build pyramid */
+	  for (row = 0; row <= h-1; row++)
+	    {
+	      /* Print spaces */
+	      for (spaces = 0; spaces < (h-row-1); spaces++)
+		{ 
+		  printf(" ");
+		}
+	      
+	      /* Print asterisks */
+	      for (stars = 1; stars < (h-spaces)*2; stars++)
+		{
+		  printf("*");
+		}
+	      
+	      printf("\n");
+	    }
+	}
     }
 }
