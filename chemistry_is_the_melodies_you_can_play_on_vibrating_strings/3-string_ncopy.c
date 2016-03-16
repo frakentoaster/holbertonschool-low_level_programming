7705dc85b47b6f6644c699e6585cf2ca90e69996
Rick Houser <21@p6-4.hbtn.io>
2016-03-16 10:59:52 -0700

    rewrite strcpy

---
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/3-string_ncopy.c
@@ -0,0 +1,16 @@
+int print_char(char c);
+
+char *string_ncopy(char *dest, const char *src, int n)
+{
+  int i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+  {
+    dest[i] = src[i];
+  }
+  for ( ; i < n; i++)
+  {
+    dest[i] = '\0';
+  }
+  return dest;
+}
