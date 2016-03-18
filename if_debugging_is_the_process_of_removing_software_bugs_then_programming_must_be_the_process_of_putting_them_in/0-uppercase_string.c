e767778e0b7f3d88f2f377af5c5e2d5d17bed3ee
FrackenToaster <frakentoaster@gmail.com>
2016-03-18 16:23:23 -0700

    alter scripts to be more explicit

--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
@@ -4,7 +4,7 @@ char *uppercase_string(char *c) {
 	/* loop til end of string */
 	for (i = 0; c[i] != '\0'; i++) {
   /* If char is within range of lowercase on ascii table */
-	  if (c[i] < 123 && c[i] > 96) {
+	  if (c[i] <= 'z' && c[i] >= 'a') {
 	    c[i]-=32; /* convert to cap in ascii table */
 	  }
 	}
--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/1-cap_string.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/1-cap_string.c
@@ -3,7 +3,7 @@ char *cap_string(char *c) {
 	/* loop til end of string */
 	for (i = 0; c[i] != '\0'; i++) {
 	  /* If char is within range of lowercase on ascii table */
-		if (c[i] < 123 && c[i] > 96) {
+		if (c[i] <= 'z' && c[i] >= 'a') {
 			/* checks previous char for space, newline and tabulation */
 		  if (c[i-1] == ' ' || c[i-1] == '\n' || c[i-1] == '\t') {
 		    c[i]-=32; /* convert to cap in ascii table */
