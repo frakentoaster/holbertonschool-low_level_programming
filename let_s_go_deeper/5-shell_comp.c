f1ec3adefc121595fe81c12a0cd3ac06a70e9226
FrackenToaster <frakentoaster@gmail.com>
2016-03-31 00:46:19 -0700

    recursive function that compares two strings, returning 1 is same and 0 if not.'*' treated as wild card

--- let_s_go_deeper/5-shell_comp.c
+++ let_s_go_deeper/5-shell_comp.c
@@ -1,21 +1,21 @@
-
-/* Function returns 1 if strings a and b are same, otherwise 0.
-It recursively increases a and b pointers.
-Using ternary operator, if *a is not equal to *b then 1 is returned.
-If we reach end of both strings at the same time then 1 is returned. */
-int shell_comp(char *a, char *b)
-{
-  if ((*b == '*' && *b + 1 == '*') || (*a == *b + 1)) {
-      return shell_comp(a, ++b);
-  }
-  if (*a != '\0' && *b == '\0'){
-    return shell_comp(++a, b);
-  }
-  if ((*a == *b && *b == '\0') || *b == '*'){
+int shell_comp(char *s1, char *s2)
+ {
+  if (*s1 == *s2 && *s2 == '\0') {
     return 1;
   }
-  if (*a == *b) {
-    return shell_comp(++a,++b);
+  else if (*s1 == *s2) {
+    return shell_comp(s1 + 1, s2 + 1);
+  }
+  else if (*s2 == '*') {
+    if (*(s2 + 1) != '\0' && *s1 == '\0') {
+      return 0;
+    }
+    else if (shell_comp(s1 + 1, s2)) {
+      return 1;
+    }
+    else {
+      return shell_comp(s1, s2 + 1);
+    }
   }
   return 0;
-}
+ }
