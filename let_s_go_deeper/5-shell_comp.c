8f627c456250eadbd23b9e46e33f7a174b57ae5b
Rick Houser <21@p6-4.hbtn.io>
2016-03-30 18:32:30 -0700

    WIP function to compare two strings

--- let_s_go_deeper/5-shell_comp.c
+++ let_s_go_deeper/5-shell_comp.c
@@ -1,8 +1,21 @@
+
 /* Function returns 1 if strings a and b are same, otherwise 0.
 It recursively increases a and b pointers.
 Using ternary operator, if *a is not equal to *b then 1 is returned.
 If we reach end of both strings at the same time then 1 is returned. */
 int shell_comp(char *a, char *b)
 {
-  return ((*a==*b&&*b=='\0')||*b=='*')?1:(*a==*b)?shell_comp(++a,++b):0;
+  if ((*b == '*' && *b + 1 == '*') || (*a == *b + 1)) {
+      return shell_comp(a, ++b);
+  }
+  if (*a != '\0' && *b == '\0'){
+    return shell_comp(++a, b);
+  }
+  if ((*a == *b && *b == '\0') || *b == '*'){
+    return 1;
+  }
+  if (*a == *b) {
+    return shell_comp(++a,++b);
+  }
+  return 0;
 }
