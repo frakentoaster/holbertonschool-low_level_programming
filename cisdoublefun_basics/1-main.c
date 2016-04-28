372eb4a3bbd844f9229bfac77b811db352b7d841
FrackenToaster <frakentoaster@gmail.com>
2016-04-27 23:59:31 -0700

    remove unneeded files

--- cisdoublefun_basics/1-main.c
+++
@@ -1,8 +0,0 @@
-int print_char(char c);
-void print_tebahpla(void);
-
-int main(void)
-{
-   print_tebahpla();
-   return (0);
-}
--- cisdoublefun_basics/3-main.c
+++
@@ -1,8 +0,0 @@
-int print_char(char c);
-void print_base16(void);
-
-int main(void)
-{
-   print_base16();
-   return (0);
-}
