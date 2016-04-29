8f4da4169c548d164df19a706ea3720e863f5c48
FrackenToaster <frakentoaster@gmail.com>
2016-04-29 14:53:30 -0700

    update

--- cisdoublefun_day_3_recursion/1-factorial.c
+++ cisdoublefun_day_3_recursion/1-factorial.c
@@ -1,7 +1,6 @@
 /* recursive function to calculate factorial */
 int factorial(int n) {
-	/* handle negative & INT_MAX/MIN edge cases */
-	if (n < 0 || n > 2147483647) {
+	if (n < 0) {
 		return (-1);
 	}
 	else if (n == 0) {
