6cc2d310088035edf30462e59c6cb47bdc1c09b4
FrackenToaster <frakentoaster@gmail.com>
2016-04-29 14:54:32 -0700

    add power.c

--- cisdoublefun_day_3_recursion/0-factorial.c
+++ cisdoublefun_day_3_recursion/0-factorial.c
@@ -3,8 +3,8 @@ int factorial(int n) {
 	int i;
 	int res;
 	i = 1;
-	/* handle negative & INT_MAX/MIN edge cases */
-	if (n < 0 || n > 2147483647) {
+
+	if (n < 0) {
 		return (-1);
 	}
 	else if (n == 0) {
