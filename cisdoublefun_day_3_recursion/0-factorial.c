d66c35d12d815b2df61c29055adf038489902b98
FrackenToaster <frakentoaster@gmail.com>
2016-04-29 17:12:03 -0700

    edit funtions to handle overflow

--- cisdoublefun_day_3_recursion/0-factorial.c
+++ cisdoublefun_day_3_recursion/0-factorial.c
@@ -4,6 +4,7 @@ int factorial(int n) {
 	int res;
 	i = 1;
 
+	/* handles n being 0 and INT_MAX (integer overflow) */
 	if (n < 0) {
 		return (-1);
 	}
@@ -12,7 +13,11 @@ int factorial(int n) {
 	}
 	else {
 		res = 1;
-		while (i < n) {
+		while (i <= n) {
+			/* avoid exceeding INT_MIN value */
+			if (res * i < -2147483647) {
+				return (-1);
+			}
 			res *= i;
 			i++;
 		}
--- cisdoublefun_day_3_recursion/1-factorial.c
+++ cisdoublefun_day_3_recursion/1-factorial.c
@@ -1,12 +1,13 @@
+#include <limits.h>
+
 /* recursive function to calculate factorial */
 int factorial(int n) {
-	if (n < 0) {
+	/* handles integer overflow (MAX & MIN) */
+	if (n < 0 || n > 12) {
 		return (-1);
 	}
 	else if (n == 0) {
 		return (1);
 	}
-	else {
-		return (n * factorial(n - 1));
-	}
+	return (n * factorial(n - 1));
 }
