7ef6b850c800081801fd6578aa2431f40a3c5041
FrackenToaster <frakentoaster@gmail.com>
2016-05-10 20:04:40 -0700

    function to reverse string without using temp variable

--- why_so_serious/0-crazy_reverse_string.c
+++ why_so_serious/0-crazy_reverse_string.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int string_len(char *s)
-{
+int string_len(char *s) {
      int l;
 
      l = 0;
-     while (*s)
-     {
+     while (*s) {
           s++;
           l++;
      }
@@ -17,24 +15,25 @@ int string_len(char *s)
 /* reverse a string
  * works for any string of size 0 to INT_MAX - 1
  */
-void crazy_reverse_string(char *s)
-{
+void crazy_reverse_string(char *s) {
      int l;
      int i;
      /* you are not allowed to declare new variables */
 
      l = string_len(s);
      i = 0;
-     while (i < l / 2)
-     {
+     while (i < l / 2) {
           /* your code goes here only */
           /* you can only use s, l and i to complete the task */
+					s[l] = s[i];
+					s[i] = s[l - i - 1];
+					s[l - i - 1] = s[l];
           i++;
      }
+		 s[l] = '\0';
 }
 
-int main(void)
-{
+int main(void) {
      char *s;
 
      s = strdup("Hello Holberton! \\o/ #cisfun"); /* This is an example, but it should work for any string */
