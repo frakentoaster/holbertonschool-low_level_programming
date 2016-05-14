0eaf28e8c766cbc334844ebbefc6a1c70388b608
FrackenToaster <frakentoaster@gmail.com>
2016-05-14 00:04:55 -0700

    rough working shell. commands that work: ls, env, echo and echo 0 (WIP)

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -3,6 +3,11 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
 
 int string_len(char *s) {
 	int l;
@@ -21,24 +26,65 @@ void print_prompt(void) {
 	print_string("Koopa$ ");
 }
 
+int print_char(char c)
+{
+  return (write(1, &c, 1));
+}
+
 int main(int ac, char **av, char **env) {
 	char *command;
 	char **argv;
 	pid_t id;
 	int status;
+	int num;
+	int st_cmd;
 
 	while (1) { /* Infinite loop til exit */
 		print_prompt();
 		command = read_line(0);
-		argv = string_split(command, ' ');
+		st_cmd = strcmp("echo $?", command);
+
 		/* Check for exit command */
-		if (strcmp("exit", argv[0]) == 0) {
+		if (strcmp("exit", command) == 0) {
 			return (0);
 		}
+
+		if (st_cmd == 0) {
+			printf("EXIT CODE YO! %d\n", WEXITSTATUS(status));
+		}
+		argv = string_split(command, ' ');
+
+		/* Set PATH based on user input */
+		if (strcmp("ls", command) == 0) {
+			argv[0] = "/bin/ls";
+		}
+
+		if (strcmp("env", argv[0]) == 0) {
+			argv[0] = "/usr/bin/env";
+		}
+		/* Handles echo commands */
+		if (strcmp("echo", argv[0]) == 0) {
+			argv[0] = "/bin/echo";
+		}
+//////////////////////////// TO DO //////////////////////////
+/* kill off multiple child shells after calling.(prevents exit) */
+/* dynamically pass in path and commands by parsing command line args */
+
+		/*int r = 0;
+		while (argv[r] != '\0'){
+			print_string(*env);
+			r++;
+		}*/
+
 		id = fork();
 		/* Fork so doesn't end after first execve */
-		if (id == 0) { /* Execute inside child */
+		if (id == 0 && st_cmd != 0) { /* Execute inside child */
 			execve(argv[0], argv, env);
+			perror("Execve Failure"); /* Execve only returns on failure */
+			exit(EXIT_FAILURE);
+		} else if (id == -1){
+			perror("Fork Failure");
+			exit(EXIT_FAILURE);
 		} else {
 			wait(&status);
 		}
