09ff2c9e6e1c275e125d6f926dde6b0e5e131957
FrackenToaster <frakentoaster@gmail.com>
2016-10-01 15:55:44 -0700

    malloc space for array

--- hash_tables/ht_create.c
+++ hash_tables/ht_create.c
@@ -14,13 +14,16 @@ HashTable *ht_create(unsigned int size) {
 	if ((ht_new = malloc(sizeof(HashTable))) == NULL)
 		return NULL;
 
-	/* Initialize elements of table */
-	for (i = 0; i < size; i++) {
-		ht_new->array[i] = NULL;
+	/* Allocate memory for list */
+	ht_new->array = malloc(sizeof(List *) * size);
+	if (ht_new->array == NULL) {
+		return NULL;
+	} else {
+		for (i = 0; i < size; i++) {
+			ht_new->array[i] = NULL;
+		}
 	}
-
 	/* Set table size */
 	ht_new->size = size;
-
 	return ht_new;
 }
