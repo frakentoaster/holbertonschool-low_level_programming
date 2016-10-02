968116d064101365d91778023b1dde2de7b3b7ba
FrackenToaster <frakentoaster@gmail.com>
2016-10-01 19:19:37 -0700

    function creates new nodes in a linked list

---
+++ hash_tables/ht_put.c
@@ -0,0 +1,67 @@
+#include "hashtable.h"
+#include <stdlib.h>
+#include <string.h>
+unsigned int hash(const char *key, unsigned int size);
+
+/* Compares two strings */
+int str_cmp(const char *str1, const char* str2)
+{
+    int i;
+    for (i = 0; str1[i] != '\0'; i++) {
+        if (str1[i] != str2[i]) {
+            return 0;
+        }
+    }
+    if (str1[i] != str2[i]) {
+        return 0;
+    }
+    return 1;
+}
+
+/* Adds an element to a HashTable */
+int ht_put(HashTable *hashtable, const char *key, const char *value) {
+  List *node;
+  unsigned int idx;
+  /* Get the 'index corresponding' to the key */
+  idx = hash(key, hashtable->size);
+  node = hashtable->array[idx];
+
+  /*
+    Iterate through list checking for duplicate keys, incrementing
+    node as we go along.
+  */
+  if (node != NULL) {
+    while (node != NULL) {
+      if (str_cmp(node->key, key)) {
+        break;
+      }
+      node = node->next;
+    }
+  }
+  /*
+    1. Check if node already exists and free allocated space
+    2. If doesn't exist, allocate memory for node.
+    3. Set corresponding key, value pairs.
+    4. Check for issues with malloc or key, val pairs.
+    5. Free allocated space if step 4 fails and return 1.
+    6. All the above passes, we set value for node->next.
+    7. Set the node as the new head node.
+  */
+  if (node != NULL) {
+    free(node->value);
+  } else {
+    node = malloc(sizeof(List));
+    node->key = strdup(key);
+    node->value = strdup(value);
+    if ((node == NULL) || (node->key == NULL) || (node->value == NULL)) {
+      free(node->key);
+      free(node->value);
+      free(node);
+      return 1;
+    } else {
+      node->next = hashtable->array[idx];
+      hashtable->array[idx] = node;
+    }
+  }
+  return 0;
+}
