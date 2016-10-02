930fef6421f046a6790536ac5b4e5df145ca5372
FrackenToaster <frakentoaster@gmail.com>
2016-10-01 20:39:41 -0700

    add function that returns a value to the corresponding key

---
+++ hash_tables/ht_get.c
@@ -0,0 +1,22 @@
+#include "hashtable.h"
+#include <stdio.h>
+unsigned int hash(const char *key, unsigned int size);
+int str_cmp(const char *str1, const char* str2);
+
+/* Iterate until we find node key that matches the given key */
+char *ht_get(HashTable *hashtable, const char *key) {
+  List *node;
+  unsigned int idx;
+  idx = hash(key, hashtable->size);
+  node = hashtable->array[idx];
+
+  if (node != NULL) {
+    while (node != NULL) {
+      if (str_cmp(node->key, key)) {
+        return node->value;
+      }
+      node = node->next;
+    }
+  }
+  return NULL;
+}
