c4af0743d3a770aa2477a4eee3253427218a6238
FrackenToaster <frakentoaster@gmail.com>
2017-01-31 09:43:31 -0800

    init commit. Print linkedlist complete and passes Betty check.

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - print all nodes and return node count.
+ * @h: head node
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node_cnt;
+
+	node_cnt = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_cnt++;
+	}
+
+	return (node_cnt);
+}
---
+++ 0x12-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef LISTS
+#define LISTS
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+		int n;
+		struct listint_s *next;
+} listint_t;
+
+/* Function prints all elements of listint_t list */
+size_t print_listint(const listint_t *h);
+#endif
