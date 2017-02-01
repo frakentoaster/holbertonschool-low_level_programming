d131a12cb7c3920275df9a6ad83443dd4811fe6c
FrackenToaster <frakentoaster@gmail.com>
2017-02-01 10:36:11 -0800

    add function to free memory in linked list

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint - Frees all blocks used in listint_t list.
+ * @head: head node pointer
+ *
+ * Description: Iterates a linked list freeing each block.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
