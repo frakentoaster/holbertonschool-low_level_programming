7301ea0e76baa2abebd89bd4b239a480ba39f3e1
FrackenToaster <frakentoaster@gmail.com>
2017-02-01 12:27:44 -0800

    add function to free memory usage and set head to NULL

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - Frees all blocks used in listint_t list.
+ * @head: pointer to head node pointer
+ *
+ * Description: Iterates a linked list freeing each block & sets head to NULL.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
+
+	curr = *head;
+	while (curr != NULL)
+	{
+		temp = curr;
+		curr = curr->next;
+		free(temp);
+	}
+	head = NULL;
+}
