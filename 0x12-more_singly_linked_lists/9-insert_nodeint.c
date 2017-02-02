bc40ad8587d19c71df7f6428967b5898ce329df9
FrackenToaster <frakentoaster@gmail.com>
2017-02-02 11:10:34 -0800

    update functiont to return pointer

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		new_node->n = n;
 		new_node->next = current_node;
 		temp_node->next = new_node;
-		return (current_node);
+		return (*head);
 	}
 	return (NULL);
 }
