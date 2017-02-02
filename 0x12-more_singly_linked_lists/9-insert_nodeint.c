87c42d3d8e0dc64cad9d65a357b9467531739350
FrackenToaster <frakentoaster@gmail.com>
2017-02-02 11:23:43 -0800

    add conditional check for adding nodes to index 0 of a linked list

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,14 +21,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (new_node != NULL)
 	{
-		for (i = 0; i < index; i++)
+		for (i = 0; i <= index; i++)
 		{
 			if (current_node == NULL)
 			{
 				return (NULL);
 			}
-			temp_node = current_node;
-			current_node = current_node->next;
+			else if (index == 0)
+			{
+				new_node->n = n;
+				new_node->next = *head;
+				*head = new_node;
+			}
+			else
+			{
+				temp_node = current_node;
+				current_node = current_node->next;
+			}
 		}
 		new_node->n = n;
 		new_node->next = current_node;
